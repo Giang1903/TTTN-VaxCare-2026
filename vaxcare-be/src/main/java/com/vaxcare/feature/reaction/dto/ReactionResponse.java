688c263289d61ecf52c6fa223ce8eb2d051ab379
HuongGiang <hgiang190325@gmail.com>
2026-09-14 14:13:41 +0700

    fix: hiển thị số mũi/lô bên staff

--- vaxcare-be/src/main/java/com/vaxcare/feature/reaction/dto/ReactionResponse.java
+++ vaxcare-be/src/main/java/com/vaxcare/feature/reaction/dto/ReactionResponse.java
@@ -19,10 +19,13 @@ public class ReactionResponse {
 
     private Long userId;
     private String userFullName;
+    private String userPhone;
 
     private Long vaccineId;
     private String vaccineName;
     private LocalDate injectionDate;
+    private Integer doseNumber;
+    private String batchNumber;
 
     private Long facilityId;
     private String facilityName;
